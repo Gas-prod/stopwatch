96f17b900eec7b1395d02d0b9b1162740f0333a9
Gas-prod <62751705+Gas-prod@users.noreply.github.com>
2022-12-29 23:54:39 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,11 +3,11 @@ toolbox.precache([
     "/stopwatch/index.html", 
     "/stopwatch/style.css",
     "/stopwatch/script.js",
-    "/stopwatch/icon/512x512.png", 
-    "/stopwatch/icon/256x256.png",
-    "/stopwatch/icon/192x192.png",
-    "/stopwatch/icon/128x128.png",
-    "/stopwatch/icon/icon.svg",
+    "/stopwatch/img/icon/512x512.png", 
+    "/stopwatch/img/icon/256x256.png",
+    "/stopwatch/img/icon/192x192.png",
+    "/stopwatch/img/icon/128x128.png",
+    "/stopwatch/img/icon/icon.svg",
 ])
 
 toolbox.router.get("/*", toolbox.networkFirst, {
