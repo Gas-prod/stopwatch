7ff8677c5c85d89bef5fea329040987ebb9e5234
Gas-prod <62751705+Gas-prod@users.noreply.github.com>
2023-01-30 02:33:49 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ toolbox.precache([
     "/stopwatch/img/icons/192x192.png",
     "/stopwatch/img/icons/128x128.png",
     "/stopwatch/img/icons/icon.svg",
-    "https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;600;700&family=Sora:wght@400;700&display=swap",
+    "https://fonts.googleapis.com/css2?family=family=Sora:wght@400;700&family=Share+Tech+Mono&display=swap",
     "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
     "/stopwatch/nosleep.js"
 ])
